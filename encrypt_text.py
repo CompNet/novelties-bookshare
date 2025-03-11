a14fc1fc738a2021052fcc2e3d1e242beecac5ee
Aethor <oow.autre@gmail.com>
2025-03-11 16:32:51 +0100

    first version of decrypt_tokens

--- encrypt_text.py
+++ encrypt_text.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from typing import List
+import functools
 import sys
 import argparse
 import os
@@ -7,39 +8,35 @@ import json
 import hashlib
 
 
+@functools.lru_cache
+def encrypt_token(token: str) -> str:
+    h = hashlib.sha256()
+    h.update(token.encode("utf-8"))
+    return h.hexdigest()
+
+
 def encrypt_tokens(tokens: List[str]) -> List[str]:
-    """Encrypts the textual content of a novel"""
-    # dictionary of hashes
-    hash_dict = {}
-
-    for token in tokens:
-        # new token type: compute hash
-        if not token in hash_dict:
-            h = hashlib.sha256()
-            h.update(token.encode("utf-8"))
-            # encrypt token type
-            hash_dict[token] = h.hexdigest()[:3]
-
-    print("# word types: {}".format(len(hash_dict)))
-    print("# hash types: {}".format(len(set(hash_dict.values()))))
-    return annotations
+    """Encrypts the textual content of a novel."""
+    return [encrypt_token(token) for token in tokens]
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--annot_file", type=str, help="Annotation file.", required=True
-    )
-    parser.add_argument("--output_file", type=str, help="Output file.", required=True)
+    parser.add_argument("--annot_file", type=str, help="Annotation file.")
+    parser.add_argument("--output_file", type=str, help="Output file.")
     args = parser.parse_args()
 
-    annot_file = os.path.expanduser(args.annot_file)
-    with open(annot_file) as f:
-        annotations = json.load(f)
+    if args.annot_files:
+        with open(os.path.expanduser(args.annot_file)) as f:
+            annotations = json.load(f)
+    else:
+        annotations = json.loads(sys.stdin.read())
 
     out_annotations = encrypt_tokens(args.annot_file)
 
-    output_file = os.path.expanduser(args.output_file)
-    with open(output_file, "w") as f:
-        json.dump(out_annotations, f, indent=2)
+    if args.output_file:
+        with open(os.path.expanduser(args.output_file), "w") as f:
+            json.dump(out_annotations, f, indent=2)
+    else:
+        print(json.dumps(out_annotations, indent=2))
