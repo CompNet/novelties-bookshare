fea312ca5ee98fce4f30615ab4e7fe5ea6533dc3
Aethor <oow.autre@gmail.com>
2025-04-21 16:52:04 +0800

    CLI interface for encrypt

---
+++ novelties_bookshare/conll.py
@@ -0,0 +1,57 @@
+from typing import Optional, Dict, List, Tuple
+import os, sys
+
+
+def load_conll2002_bio(
+    path: Optional[str] = None, separator: str = " ", **kwargs
+) -> Tuple[List[str], List[str]]:
+    """Load a file under CoNLL2022 BIO format.
+
+    :param path: path to the CoNLL-2002 formatted file.  If ``None``,
+        read from stdin.
+    :param separator: separator between token and BIO tags
+    :param kwargs: additional kwargs for :func:`open` (such as
+        ``encoding`` or ``newline``).
+
+    :return: ``(tokens, tags)``
+    """
+    if not path is None:
+        with open(os.path.expanduser(path), **kwargs) as f:
+            raw_data = f.read()
+    else:
+        raw_data = sys.stdin.read()
+
+    tokens = []
+    tags = []
+    for line in raw_data.split("\n"):
+        line = line.strip("\n")
+        token, tag = line.split(separator)
+        tokens.append(token)
+        tags.append(tag)
+
+    return tokens, tags
+
+
+def dump_conll2002_bio(
+    tokens: List[str],
+    tags: List[str],
+    path: Optional[str] = None,
+    separator: str = " ",
+    **kwargs,
+):
+    """Dump a list of tokens/tags in the CoNLL-2002 format.
+
+    :param path: path to the output file.  If ``None``, print to stdout.
+    :param separator: separator between token and BIO tags
+    :param kwargs: additional kwargs for :func:`open` (such as
+        ``encoding`` or ``newline``).
+    """
+    assert len(tokens) == len(tags)
+
+    if not path is None:
+        with open(path, "w", **kwargs) as f:
+            for token, tag in zip(tokens, tags):
+                f.write(f"{token}{separator}{tag}\n")
+    else:
+        for token, tag in zip(tokens, tags):
+            print(f"{token}{separator}{tag}")
--- novelties_bookshare/encrypt.py
+++ novelties_bookshare/encrypt.py
@@ -1,42 +1,39 @@
 #!/usr/bin/python3
-from typing import List
+from typing import List, Optional
 import functools
-import sys
 import argparse
-import os
-import json
 import hashlib
+
+from novelties_bookshare.conll import dump_conll2002_bio, load_conll2002_bio
 
 
 @functools.lru_cache
-def encrypt_token(token: str) -> str:
+def encrypt_token(token: str, hash_len: Optional[int] = None) -> str:
     h = hashlib.sha256()
     h.update(token.encode("utf-8"))
+    if not hash_len is None:
+        return h.hexdigest()[:hash_len]
     return h.hexdigest()
 
 
-def encrypt_tokens(tokens: List[str]) -> List[str]:
-    """Encrypts the textual content of a novel."""
-    return [encrypt_token(token) for token in tokens]
+def encrypt_tokens(tokens: List[str], hash_len: Optional[int] = None) -> List[str]:
+    return [encrypt_token(token, hash_len) for token in tokens]
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--annot_file", type=str, help="Annotation file.")
-    parser.add_argument("--output_file", type=str, help="Output file.")
+    parser.add_argument("-i", "--input-file", type=str, help="Input CoNLL-2002 file.")
+    parser.add_argument(
+        "-s",
+        "--separator",
+        type=str,
+        default=" ",
+        help="Separator between tokens and BIO tags.",
+    )
+    parser.add_argument("-o", "--output-file", type=str, help="Output CoNLL-2002 file.")
     args = parser.parse_args()
 
-    if args.annot_files:
-        with open(os.path.expanduser(args.annot_file)) as f:
-            annotations = json.load(f)
-    else:
-        annotations = json.loads(sys.stdin.read())
-
-    out_annotations = encrypt_tokens(args.annot_file)
-
-    if args.output_file:
-        with open(os.path.expanduser(args.output_file), "w") as f:
-            json.dump(out_annotations, f, indent=2)
-    else:
-        print(json.dumps(out_annotations, indent=2))
+    tokens, tags = load_conll2002_bio(args.input_file, separator=args.separator)
+    encrypted_tokens = encrypt_tokens(tokens)
+    dump_conll2002_bio(tokens, tags, args.output_file, args.separator)
