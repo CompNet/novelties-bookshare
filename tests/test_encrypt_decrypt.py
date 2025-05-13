cd2fb990267999aca79dedfcd2b2cbb8e4852488
Aethor <oow.autre@gmail.com>
2025-05-13 16:55:01 +0800

    add more tests

--- tests/test_encrypt_decrypt.py
+++ tests/test_encrypt_decrypt.py
@@ -1,12 +1,27 @@
+from typing import List
 from novelties_bookshare.encrypt import encrypt_tokens
-from novelties_bookshare.decrypt import decrypt_tokens
+from novelties_bookshare.decrypt import decrypt_tokens, decryptplugin_propagate
+from hypothesis import given, strategies as st
 
 
 def test_substitution():
     ref_tokens = "A B C D E E".split()
-    user_tokens = "A B C D X E".split()
+    user_tokens = "A B C D E X".split()
     tags = "B-PER O O O B-PER I-PER".split()
     pred_tokens = decrypt_tokens(encrypt_tokens(ref_tokens), tags, user_tokens)
+    assert pred_tokens == "A B C D E [UNK]".split()
+
+
+def test_substitution_propagate():
+    ref_tokens = "A B C D E E".split()
+    user_tokens = "A B C D E X".split()
+    tags = "B-PER O O O B-PER I-PER".split()
+    pred_tokens = decrypt_tokens(
+        encrypt_tokens(ref_tokens),
+        tags,
+        user_tokens,
+        decryption_plugins=[decryptplugin_propagate],
+    )
     assert pred_tokens == ref_tokens
 
 
@@ -24,3 +39,9 @@ def test_addition():
     tags = "B-PER O O O B-PER I-PER".split()
     pred_tokens = decrypt_tokens(encrypt_tokens(ref_tokens), tags, user_tokens)
     assert pred_tokens == ref_tokens
+
+
+@given(st.lists(st.text()))
+def test_encrypt_decrypt_recover_original_tokens(tokens: List[str]):
+    tags = ["O"] * len(tokens)
+    assert decrypt_tokens(encrypt_tokens(tokens), tags, tokens) == tokens
