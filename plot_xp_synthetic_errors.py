cc8c72c8dcb514d4197a329679db3d947757c329
aethor <oow.autre@gmail.com>
2025-10-31 10:46:40 +0800

    improve xp_synthetic_errors plotting, add OCR errors plotting

--- plot_xp_synthetic_errors.py
+++ plot_xp_synthetic_errors.py
@@ -23,9 +23,38 @@ def get_params(metric_key: str) -> dict[str, str]:
     }
 
 
+def load_metrics(run: pl.Path) -> dict:
+    # load metrics
+    with open(args.run / "metrics.json") as f:
+        metrics = json.load(f)
+    # some metrics are not ordered in terms of steps
+    for v in metrics.values():
+        sort_idx = sorted(list(range(len(v["steps"]))), key=lambda i: v["steps"][i])
+        for k in v.keys():
+            v[k] = [v[k][i] for i in sort_idx]
+    return metrics
+
+
+METRIC2PRETTY = {
+    "errors_nb": "Number of errors",
+    "duration_s": "Duration (s)",
+    "errors_percent": "Percentage of errors",
+    "entity_errors_nb": "Number of entity errors",
+    "entity_errors_percent": "Percentage of entity errors",
+}
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-r", "--run", type=pl.Path)
+    parser.add_argument(
+        "-r", "--run", type=pl.Path, help="Run of xp_synthetic_errors.py"
+    )
+    parser.add_argument(
+        "-c",
+        "--ocr-run",
+        type=pl.Path,
+        default=None,
+        help="Run of xp_synthetic_ocr_errors.py",
+    )
     parser.add_argument(
         "-m",
         "--metric",
@@ -35,14 +64,12 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--output-dir", type=pl.Path)
     args = parser.parse_args()
 
-    # load metrics
-    with open(args.run / "metrics.json") as f:
-        metrics = json.load(f)
-    # some metrics are not ordered in terms of steps???
-    for v in metrics.values():
-        sort_idx = sorted(list(range(len(v["steps"]))), key=lambda i: v["steps"][i])
-        for k in v.keys():
-            v[k] = [v[k][i] for i in sort_idx]
+    metrics = load_metrics(args.run)
+    if args.ocr_run:
+        ocr_run_metrics = load_metrics(args.ocr_run)
+        metrics = {**ocr_run_metrics, **metrics}
+    else:
+        print("OCR run not specified. Not plotting OCR results.")
 
     # construct df
     df_dict = defaultdict(list)
@@ -62,6 +89,7 @@ if __name__ == "__main__":
 
     # plot
     os.makedirs(args.output_dir, exist_ok=True)
+
     # one subplot per "noise"
     # pick "book" to split curves
     for strat in set(df["strat"]):
@@ -70,6 +98,7 @@ if __name__ == "__main__":
         fig.suptitle(strat)
         for i, noise in enumerate(noises):
             ax = axs[i // 2][i % 2]
+            ax.set_ylabel(METRIC2PRETTY[args.metric])
             ax_df = df[
                 (df["strat"] == strat)
                 & (df["noise"] == noise)
@@ -79,17 +108,20 @@ if __name__ == "__main__":
                 ax_df[ax_df["book"] == book].plot(  # type: ignore
                     ax=ax, x="steps", y="values", title=noise, label=book
                 )
+        plt.tight_layout()
         out_path = args.output_dir / f"perbook_{strat}.png"
         print(f"saving {out_path}")
         plt.savefig(out_path)
         plt.close("all")
-    # pick or "strat" to split curves
+
+    # pick "strat" to split curves
     for book in set(df["book"]):
         noises = list(set(df["noise"]))
         fig, axs = plt.subplots(math.ceil(len(noises) / 2), 2, figsize=(12, 8))
         fig.suptitle(book)
         for i, noise in enumerate(noises):
             ax = axs[i // 2][i % 2]
+            ax.set_ylabel(METRIC2PRETTY[args.metric])
             ax_df = df[
                 (df["book"] == book) & (df["noise"] == noise) & (df["hash_len"] == "64")
             ]
@@ -97,6 +129,7 @@ if __name__ == "__main__":
                 ax_df[ax_df["strat"] == strat].plot(  # type: ignore
                     ax=ax, x="steps", y="values", title=noise, label=strat
                 )
+        plt.tight_layout()
         out_path = args.output_dir / f"perstrat_{book}.png"
         print(f"saving {out_path}")
         plt.savefig(out_path)
