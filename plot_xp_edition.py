89542cda0b25a762d4d610aa89d3647ec22d0020
aethor <oow.autre@gmail.com>
2025-10-31 15:52:33 +0800

    plot_xp_edition now support other types of xp_edition

--- plot_xp_edition.py
+++ plot_xp_edition.py
@@ -17,6 +17,30 @@ def get_params(metric_key: str) -> tuple[str, dict[str, str]]:
     return metric_name, {"strategy": strat, "edition": edition}
 
 
+def get_params_mlm(metric_key: str) -> tuple[str, dict[str, str]]:
+    # form of each metric
+    # w=window.e=edition.metric_name
+    m = re.match(r"w=([^\.]+)\.e=([^\.]+)\.(.*)", metric_key)
+    if m is None:
+        return "", {}
+    window, edition, metric_name = m.groups()
+    return metric_name, {"window": window, "edition": edition}
+
+
+def get_params_split(metric_key: str) -> tuple[str, dict[str, str]]:
+    # form of each metric
+    # t=max_token_len.s=max_split_nb.e=edition.metric_name
+    m = re.match(r"t=([^\.]+)\.s=([^\.]+)\.e=([^\.]+)\.(.*)", metric_key)
+    if m is None:
+        return "", {}
+    max_token_len, max_split_nb, edition, metric_name = m.groups()
+    return metric_name, {
+        "max_token_len": max_token_len,
+        "max_split_nb": max_split_nb,
+        "edition": edition,
+    }
+
+
 def format_bar_height(bar_value: Union[int, float]) -> str:
     if isinstance(bar_value, float):
         return f"{bar_value:.2f}"
@@ -31,6 +55,18 @@ METRIC_TO_YLABEL = {
     "entity_errors_percent": "Percentage of entity errors",
 }
 
+XP_PARAMS_KEY = {
+    "xp_edition": ["strategy", "edition"],
+    "xp_edition_mlm_params": ["window", "edition"],
+    "xp_edition_split_params": ["max_token_len", "max_split_nb", "edition"],
+}
+
+XP_GET_PARAMS_FN = {
+    "xp_edition": get_params,
+    "xp_edition_mlm_params": get_params_mlm,
+    "xp_edition_split_params": get_params_split,
+}
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-r", "--run", type=pl.Path)
@@ -43,6 +79,10 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--output-dir", type=pl.Path)
     args = parser.parse_args()
 
+    with open(args.run / "run.json") as f:
+        run_data = json.load(f)
+    xp_name = run_data["experiment"]["name"]
+
     df_dict = defaultdict(list)
     with open(args.run / "metrics.json") as f:
         data = json.load(f)
@@ -50,15 +90,12 @@ if __name__ == "__main__":
         lines = defaultdict(dict)
         for key, metric_dict in data.items():
             metric_name, params = get_params(key)
-            params_key = (
-                params["strategy"],
-                params["edition"],
-            )
+            params_key = tuple(params[k] for k in XP_PARAMS_KEY[xp_name])
             lines[params_key][metric_name] = metric_dict["values"][0]
 
-        for (strat, edition), metric_dict in lines.items():
-            df_dict["strategy"].append(strat)
-            df_dict["edition"].append(edition)
+        for params, metric_dict in lines.items():
+            for param_name, param_value in zip(XP_PARAMS_KEY[xp_name], params):
+                df_dict[param_name].append(param_value)
             for key, value in metric_dict.items():
                 df_dict[key].append(value)
 
